36842fab301c372a1a082eb261734c1ece5ba710
lekebanty <lekeadedoyin1@gmail.com>
2024-08-15 15:50:33 -0300

    Models created

---
+++ Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Virtual_Ticket.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; } // class identifier
+
+        public string ProfilePictureURl { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+
+    }
+}
---
+++ Models/Cinema.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Virtual_Ticket.Models
+{
+    public class Cinema
+    {
+        [Key]  //class Identifier
+        public int Id { get; set; }
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
